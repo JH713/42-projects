0d180240a1e7e82f4e150a31bf866d3f8e5c7b6b
Jihyeon Lee <strider13@snu.ac.kr>
2023-06-10 14:29:59 +0900

    수정

--- cpp/01/ex04/main.cpp
+++ cpp/01/ex04/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
@@ -6,4 +8,13 @@ int main(int argc, char *argv[])
 	{
 		std::cout<<"Error: Invalid number of arguments."<<std::endl;
 	}
+	std::string filename = argv[1];
+	std::string newFilename = filename + ".replace"; 
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	std::ifstream ifs;
+	ifs.open(filename);
+	std::ofstream ofs;
+	ofs.open(newFilename);
 }
