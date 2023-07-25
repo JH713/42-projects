166377898aca389c823161e5ac8372fb8b9b4488
Jihyeon Lee <strider13@snu.ac.kr>
2023-07-25 20:06:04 +0900

    ex01에서 Wrong 클래스 삭제

--- cpp/04/ex01/main.cpp
+++ cpp/04/ex01/main.cpp
@@ -1,15 +1,23 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
-#include "WrongCat.hpp"
+#include "Brain.hpp"
+
+void leaks(void)
+{
+	system("leaks brain");
+}
 
 int main()
 {
+	atexit(leaks);
 	int animalNum = 4;
 	Animal *animals = new Animal[animalNum];
 	for (int i = 0; i < animalNum / 2; i++)
 		animals[i] = Dog();
 	for (int i = animalNum / 2; i < animalNum; i++)
-		animals[i] = Dog();
-	
+		animals[i] = Cat();
+	for (int i = 0; i < animalNum; i++)
+		animals[i].getType();
+	delete []animals;
 	return (0);
 }
